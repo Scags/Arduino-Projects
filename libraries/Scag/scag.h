d7b4cfb6a58a6de75f2dfedc50b14e949dcd7e98
John Mascagni <johnmascagni@gmail.com>
2018-09-19 19:08:50 -0500

    Add files via upload

--- libraries/Scag/scag.h
+++ libraries/Scag/scag.h
@@ -1,37 +1,53 @@
 #ifndef SCAG
 #define SCAG
 
+#define repeat(a) 	for (uint16_t i = 0; i < a; ++i)
+
 /**
- *	@brief Power an array of pins.
+ *	@brief digitalWrite to an array of pins.
  *
  *	@param pins 				Array of pin indices to write to.
+ *	@param len 					Size of pins array.
  *	@param val 					Voltage to write to pins
- *	@param wait 				if a delay is needed, passed optionally.
  *
  *	@noreturn
 */
-void writePins(uint8_t pins[], size_t len, int val, int wait = 0)
+void digitalWriteEx(int pins[], size_t len, int val)
 {
 	uint8_t i;
 
 	for (i = 0; i < len; i++)
 		digitalWrite(pins[i], val);
-
-	if (wait)
-		delay(wait);
 }
 
 /**
- *	@brief Read the input of an array of pins.
+ *	@brief analogWrite to an array of pins.
+ *
+ *	@param pins 				Array of pin indices to write to.
+ *	@param len 					Size of pins array.
+ *	@param val 					Voltage to write to pins
+ *
+ *	@noreturn
+*/
+void analogWriteEx(int pins[], size_t len, int val)
+{
+	uint8_t i;
+
+	for (i = 0; i < len; i++)
+		analogWrite(pins[i], val);
+}
+
+/**
+ *	@brief digitalRead the input of an array of pins.
  *
  *	@param pins 				Array of pin indices to read from.
- *	@param wait 				If a delay is needed, passed optionally.
+ *	@param len 					Size of pins array.
  *
- *	@return 					Pointer to input array. This MUST be free()'d.
+ *	@return 					Pointer to input array.
 */
-int *readPins(uint8_t pins[], size_t len, int wait = 0)
+int *digitalReadEx(int pins[], size_t len)
 {
-	int *p = (int *) malloc(len * sizeof(int));
+	int *p = ( int * ) malloc(len * sizeof(int));
 	if (!p)
 		return NULL;
 
@@ -40,31 +56,29 @@ int *readPins(uint8_t pins[], size_t len, int wait = 0)
 	for (i = 0; i < len; i++)
 		p[i] = digitalRead(pins[i]);
 
-	if (wait)
-		delay(wait);
-
 	return p;
 }
 
 /**
- *	@brief Read the input of an array of pins and writes it to an array.
+ *	@brief analogRead the input of an array of pins.
  *
- *	@param pins 				Array of pins indices to read from.
- *	@param pins2 				Array of pin reads to write to.
- *	@param len 					Size of the first array passed.
- *	@param wait 				If a delay is needed, passed optionally.
+ *	@param pins 				Array of pin indices to read from.
+ *	@param len 					Size of pins array.
  *
- *	@noreturn
+ *	@return 					Pointer to input array.
 */
-void readPinsEx(uint8_t pins[], size_t len, uint8_t pins2[], int wait = 0)
+int *analogReadEx(int pins[], size_t len)
 {
+	int *p = ( int * ) malloc(len * sizeof(int));
+	if (!p)
+		return NULL;
+
 	uint8_t i;
 
 	for (i = 0; i < len; i++)
-		pins2[i] = digitalRead(pins[i]);
+		p[i] = analogRead(pins[i]);
 
-	if (wait)
-		delay(wait);
+	return p;
 }
 
 /**
@@ -72,10 +86,45 @@ void readPinsEx(uint8_t pins[], size_t len, uint8_t pins2[], int wait = 0)
  *
  *	@param low 					Lowest number.
  *	@param high 				Highest number.
+ *
+ *	@return 					Random number between inclusive low and inclusive high
 */
-long randomInt(int low, int high)
+long randomInt(long low, long high)
 {
 	return random(low, high+1);
+}
+
+/**
+ *	@brief Set the pinMode of an array of pins.
+ *
+ *	@param pins 				Array of pins
+ *	@param len 					Size of pin array.
+ *	@param val 					INPUT or OUTPUT.
+ *
+ *	@noreturn
+*/
+void pinModeEx(int pins[], size_t len, int val)
+{
+	uint8_t i;
+	for (i = 0; i < len; i++)
+		pinMode(pins[i], val);
+}
+
+/**
+ *	@brief Clamp a value between 2 other values.
+ *
+ *	@param val 				Pointer to the value being clamped.
+ *	@param low 				Low value.
+ *	@param high 			High value.
+ *
+ *	@noreturn
+*/
+void clamp(long *val, long low, long high)
+{
+	if (val < low)
+		val = low;
+	else if (val > high)
+		val = high;
 }
 
 #endif 	// SCAG
